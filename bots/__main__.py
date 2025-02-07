e8c17cb07d30c0030386ac170404b83b7966614e
Chris Elias <chris@chriselias.me>
2025-02-06 22:09:32 -0500

    removed code not needed related to discord

--- bots/__main__.py
+++ bots/__main__.py
@@ -1,57 +1,6 @@
-import asyncio
-import logging
-
-from .settings import (
-    DISCORD_TOKEN_PRICING,
-    DISCORD_TOKEN_TVL,
-    DISCORD_TOKEN_FEES,
-    DISCORD_TOKEN_REWARDS,
-    DISCORD_TOKEN_COMMANDER,
-    TOKEN_ADDRESS,
-    STABLE_TOKEN_ADDRESS,
-    PROTOCOL_NAME,
-)
-from .data import Token
-from .helpers import (
-    LOGGING_HANDLER,
-    LOGGING_LEVEL,
-)
-from .price import PriceBot
-from .tvl import TVLBot
-from .fees import FeesBot
-from .rewards import RewardsBot
-from .commander import CommanderBot
-
-
-async def main():
-    """Run configured bots"""
-
-    # configure discord logging handler
-    discord_logger = logging.getLogger("discord")
-    discord_logger.setLevel(LOGGING_LEVEL)
-    discord_logger.addHandler(LOGGING_HANDLER)
-
-    token = await Token.get_by_token_address(TOKEN_ADDRESS)
-    stable = await Token.get_by_token_address(STABLE_TOKEN_ADDRESS)
-
-    bot_specs = [
-        (DISCORD_TOKEN_PRICING, PriceBot(source_token=token, target_token=stable)),
-        (DISCORD_TOKEN_TVL, TVLBot(protocol_name=PROTOCOL_NAME)),
-        (DISCORD_TOKEN_FEES, FeesBot(protocol_name=PROTOCOL_NAME)),
-        (DISCORD_TOKEN_REWARDS, RewardsBot(protocol_name=PROTOCOL_NAME)),
-        (DISCORD_TOKEN_COMMANDER, CommanderBot()),
-    ]
-
-    # only run bots that have discord tokens configured
-    tasks = list(
-        map(
-            lambda spec: spec[1].start(spec[0]),
-            filter(lambda spec: spec[0], bot_specs),
-        )
-    )
-
-    await asyncio.gather(*tasks)
-
+from fastapi import FastAPI
+import uvicorn
+from .api import app
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    uvicorn.run(app, host="0.0.0.0", port=5000)
--- bots/ui/__init__.py
+++
@@ -1,3 +0,0 @@
-from .pools import PoolsDropdown  # noqa
-from .pool_stats import PoolStats  # noqa
-from .emojis import Emojis  # noqa
